857048bd7c74acc622cc70617f88e0467a1fded2
WashingtonDDS <washington.DDS@hotmail.com>
2024-09-13 09:03:22 -0300

    delete: excluir linha de codigo que não estava sendo mais utilizada linha 23

--- src/controlador/2.eventoControlador/4.criarUmResumo.ts
+++ src/controlador/2.eventoControlador/4.criarUmResumo.ts
@@ -20,8 +20,6 @@ export const criarUmResumo = async (req: Request, res: Response) => {
       )}`
     );
 
-    const descricaoProvisoria = "Gemini";
-
     const resumoCriado: TResumo = {
       usuarioId: id,
       materiaId: materiaId,
